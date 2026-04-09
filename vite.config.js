480fba840235409f576635d0c75a0d9915cc2f35
jasonching95 <vthsjd@gmail.com>
2026-04-09 04:30:23 -0700

    updated node version

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173, // default Vite port
+    strictPort: false,
+  }
 })
